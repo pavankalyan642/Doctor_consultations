6e73ae96ef329ecce9641cf9260d9f236fdc69bf
pavan kalyan <pavankalyan@gmail.com>
2024-06-19 16:55:30 +0530

    intial first commit

---
+++ src/main/java/in/ashokit/springmvc1/BestStoreApplication.java
@@ -0,0 +1,13 @@
+package in.ashokit.springmvc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestStoreApplication.class, args);
+	}
+
+}
